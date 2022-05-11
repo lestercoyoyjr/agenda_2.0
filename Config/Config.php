344ab2ba0de9dd61c4980fc307018739480b18cd
lestercoyoyjr <lester_cch@hotmail.com>
2022-05-10 21:44:27 -0600

    db connection

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,8 @@
 <?php
-const base_url = "http://localhost/eventos/";
+const base_url = "http://localhost/agenda_2.0/";
 const host = "localhost";
 const user = "root";
 const pass = "";
-const db = "eventos";
+const db = "agendas";
 const charset = "charset=utf8";
 ?>
