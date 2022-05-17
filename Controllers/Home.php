4c8989e132827f9385b15ec231deb3c4af34dceb
lestercoyoyjr <lester_cch@hotmail.com>
2022-05-16 18:20:31 -0600

    Files for full_calendar
    These are the files required to initiate full calendar

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -4,4 +4,7 @@ class Home extends Controller
     public function __construct() {
         parent::__construct();
     }
+    public function index(){
+        $this->views->getView($this, 'index');
+    }
 }
---
+++ Views/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="<?php echo base_url;?>Assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url;?>Assets/css/main.min.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="<?php echo base_url;?>Assets/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url;?>Assets/js/main.min.js"></script>
+    <script src="<?php echo base_url;?>Assets/js/moment.js"></script>
+    <script src="<?php echo base_url;?>Assets/js/sweetalert2.all.min.js"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
